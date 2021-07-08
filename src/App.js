0bfbe7b4d0c3bd6bcb613c9a55190d1386fbc6bc
ernestmatenjwa <alexmatenjwa@gmail.com>
2021-07-09 00:09:34 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div class="resume">
+      <div class="resume_left">
+      <div class="resume_profile">
+      <img src="" alt="profile_pic"></img>
+      </div>
+      <div class="resume_content">
+      <div class="resume_item resume_info">
+      <div class="title">
+           <p class="bold">ERNEST MATENJWA</p>
+           <p class="regular">SOFTWARE DEVELOPER</p>
+      </div>
+      </div>
+      </div>
+      </div>
+
+      <div class="resume_right">
+      <div class="resume_item resume_about">
+        <div class="title">
+        <p class="bold">PERSONAL ATTRIBUTES</p>
+        </div>
+        <p>I regard myself as a person who is fascinated by challenges in all
+sorts of way, work or life, that is merely based on my belief that
+evolving and adapting to challenges is the whole concept of being
+competitive and that is what i want to bring to the workplace. With
+the less experience that i have only means i have more room for
+improvement especially in programming languages, I have taught
+myself a few and am willing to learn and be productive as a
+software developer.
+</p>
+<br></br>
+<p class="bold">EDUCATION</p>
+<p>
+  
+</p>
+
+
+      </div>
+      </div>
+    </div>
+  );
+}
