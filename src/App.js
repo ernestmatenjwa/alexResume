ac4b8fefbeac8a374a1a2b753c4a39058dfad562
ernestmatenjwa <alexmatenjwa@gmail.com>
2021-07-09 23:48:09 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -64,21 +64,34 @@ export default function App() {
       <div class="resume_item resume_about">
         <div class="title">
         <p class="bold">PERSONAL ATTRIBUTES</p>
+        <hr></hr>
         </div>
         <p>I regard myself as a person who is fascinated by challenges in all sorts of way, work or life, that is merely based on my belief that evolving and adapting to challenges is the whole concept of being competitive and that is what i want to bring to the workplace. With the less experience that i have only means i have more room for improvement especially in programming languages, I have taught myself a few and am willing to learn and be productive as a software developer.</p>
 <br></br>
 <p class="bold">EDUCATION</p>
+<hr></hr><br></br>
 <p class="min_head"><b>TERTIARY</b></p>
 <p class="minT">Institution: TSHWANE UNIVERSITY OF TECHNOLOGY</p>
 <p>Course &emsp;: NDip: information Technology: Software development</p>
 <p>Faculty &emsp;: Information & Communication Technology(ICT)</p>
 <p>Level &emsp;&emsp;: final level</p>
 <p class="min_head"><b>HIGH SCHOOL</b></p>
-<p>School name :  SIBUSISIWE HIGH SCHOOL</p>
+<p>School name&emsp;&emsp;&emsp;&emsp;&emsp; :  SIBUSISIWE HIGH SCHOOL</p>
+<p>Highest grade passed&emsp;: 12</p>
+<p>completion  &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;: 2015</p>
+<p>Subject passed &emsp;&emsp;&emsp;&emsp; : Siswati Home Language,</p>
+<p>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;English First Additional language,</p>
+<p>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Mathematical literacy, Life Orientation,</p>
+<p>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Agricultural Science, Geography,</p>
+<p>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Life Sciences</p>                                           
+<br></br>
+<p class="bold">REFERENCE</p>
+<hr></hr><br></br>
 
-<p></p>
-<p></p>
-<p></p>
+<p>Mr. Ranko V.N</p>
+<p>Lecturer & WIL Coordinator</p>
+<p>Tell     : 012 382 9112</p>
+<p>Email : rankovn@tut.ac.za</p>
       </div>
       </div>
     </div>
